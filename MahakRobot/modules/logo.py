0dc1a26952b994b45cc6f1df53026f2d8d846377
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-29 14:07:38 +0530

    Update logo.py

--- MahakRobot/modules/logo.py
+++ MahakRobot/modules/logo.py
@@ -435,7 +435,7 @@ async def lego(event):
     quew = event.pattern_match.group(1)
     if event.sender_id != OWNER_ID and not quew:
         await event.reply(
-            "๏`ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ ʙᴀʙᴇ​ !`\n๏`ᴇxᴀᴍᴘʟᴇ /logo <ʀᴏʏ-ᴇᴅɪᴛx>`"
+            "๏`ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ ʙᴀʙᴇ​ !`\n๏`ᴇxᴀᴍᴘʟᴇ /logo <ɢᴏᴅ ʀᴀᴠᴀɴ>`"
         )
         return
     pesan = await event.reply("**๏ ᴄʀᴇᴀᴛɪɴɢ ʏᴏᴜʀ ʟᴏɢᴏ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ᴀ sᴇᴄ​...**")
