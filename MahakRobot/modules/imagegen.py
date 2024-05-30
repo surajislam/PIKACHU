bacc4403918c18c1723abc98d3a43262b675c7ba
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-30 22:45:57 +0530

    Update imagegen.py

--- MahakRobot/modules/imagegen.py
+++ MahakRobot/modules/imagegen.py
@@ -8,41 +8,6 @@ from MahakRobot import pbot as app
 from pyrogram import filters
 from pyrogram.types import InputMediaPhoto
 
-@app.on_message(filters.command(["pinrstimg"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
-async def pinterest(_, message):
-     chat_id = message.chat.id
-
-     try:
-       query= message.text.split(None,1)[1]
-     except:
-         return await message.reply("**ɢɪᴠᴇ ɪᴍᴀɢᴇ ɴᴀᴍᴇ ғᴏʀ sᴇᴀʀᴄʜ 🔍**")
-
-     images = get(f"https://aiimage.hellonepdevs.workers.dev/?prompt={query}&state=url").json()
-
-     media_group = []
-     count = 0
-
-     msg = await message.reply(f"sᴄʀᴀᴘɪɴɢ ɪᴍᴀɢᴇs ғʀᴏᴍ ᴘɪɴᴛᴇʀᴇᴛs...")
-     for url in images["images"][:6]:
-
-          media_group.append(InputMediaPhoto(media=url))
-          count += 1
-          await msg.edit(f"=> ᴏᴡᴏ sᴄʀᴀᴘᴇᴅ ɪᴍᴀɢᴇs {count}")
-
-     try:
-
-        await app.send_media_group(
-                chat_id=chat_id, 
-                media=media_group,
-                reply_to_message_id=message.id)
-        return await msg.delete()
-
-     except Exception as e:
-           await msg.delete()
-           return await message.reply(f"ᴇʀʀᴏʀ : {e}")
-
-
-
 
 @app.on_message(filters.command("bingimg"))
 async def bingimg_search(client: Client, message: Message):
