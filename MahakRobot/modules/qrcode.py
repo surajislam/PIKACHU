dc148caa7811ce94c4a9f502d465b61afc509faa
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-01 02:01:32 +0530

    Update qrcode.py

--- MahakRobot/modules/qrcode.py
+++ MahakRobot/modules/qrcode.py
@@ -42,7 +42,7 @@ def qr_handler(client, message: Message):
     if len(command_text) > 1:
         input_text = " ".join(command_text[1:])
         qr_image = generate_qr_code(input_text)
-        message.reply_photo(qr_image, caption="✦ ǫʀ ᴄᴏᴅᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ɢᴇɴʀsᴛᴇᴅ ✦\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠  ‌ ๛ᴍ ᴀ ʜ ᴀ ᴋ ♡゙ ", reply_markup=InlineKeyboardMarkup(EVAA),)
+        message.reply_photo(qr_image, caption="✦ ǫʀ ᴄᴏᴅᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇ ✦\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠  ‌ ๛ᴍ ᴀ ʜ ᴀ ᴋ ♡゙ ", reply_markup=InlineKeyboardMarkup(EVAA),)
 
     else:
-        message.reply_text("✦ Please provide the text for the QR code after the command. Example usage ➠ /qr text")
+        message.reply_text("✦ ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇ ᴛᴇxᴛ ғᴏʀ ᴛʜᴇ ǫʀ ᴄᴏᴅᴇ ᴀғᴛᴇʀ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ. ᴇxᴀᴍᴘʟᴇ ᴜsᴀɢᴇ ➠ /qr ᴛᴇxᴛ")
