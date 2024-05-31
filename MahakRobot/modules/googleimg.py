fa2e53c2e4cd80fa61be942dd83c2812ad42100b
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-31 08:01:13 +0530

    Update googleimg.py

--- MahakRobot/modules/googleimg.py
+++ MahakRobot/modules/googleimg.py
@@ -1,53 +1,47 @@
 import os
-import shutil
 import glob
-from re import findall
 from bing_image_downloader import downloader
-from telethon import *
-from telethon.tl import functions
-from telethon.tl import types
-from telethon.tl.types import *
-
+from telethon import events
+from telethon.tl import functions, types
 from MahakRobot import telethn
 from MahakRobot.events import register
 
-
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (await telethn(functions.channels.GetParticipantRequest(chat, user))).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
-        )
+        participant = (await telethn(functions.channels.GetParticipantRequest(chat, user))).participant
+        return isinstance(participant, (types.ChannelParticipantAdmin, types.ChannelParticipantCreator))
     elif isinstance(chat, types.InputPeerChat):
-
-        ui = await client.get_peer_id(user)
-        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id))) \
-            .full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
-        )
+        ui = await telethn.get_peer_id(user)
+        ps = (await telethn(functions.messages.GetFullChatRequest(chat.chat_id))).full_chat.participants.participants
+        participant = next((p for p in ps if p.user_id == ui), None)
+        return isinstance(participant, (types.ChatParticipantAdmin, types.ChatParticipantCreator))
     else:
         return None
 
-
-@register(pattern="^/gimg (.*)")
+@register(pattern="^/pimg (.*)")
 async def img_sampler(event):
-     if event.fwd_from:
+    if event.fwd_from:
         return
-     if event.is_group:
-       if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-          await event.reply("♥︎ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ, ʏᴏᴜ ᴄᴀɴ'ᴛ ᴜsᴇ ᴛʜɪs ᴄᴍᴅ,, ʙᴜᴛ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ɪɴ ᴍʏ ᴘᴍ.")
-          return
-     query = event.pattern_match.group(1)
-     jit = f'"{query}"'
-     downloader.download(jit, limit=5, output_dir='store', adult_filter_off=False, force_replace=False, timeout=60)
-     os.chdir(f'./store/"{query}"')
-     types = ('*.png', '*.jpeg', '*.jpg') # the tuple of file types
-     filesgrabbed = []
-     for files in types:
-         filesgrabbed.extend(glob.glob(files))
-     await event.client.send_file(event.chat_id, filesgrabbed, reply_to=event.id)
-     os.remove(filesgrabbed)
-     os.chdir('./')
+    if event.is_group:
+        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+            await event.reply("♥︎ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ, ʏᴏᴜ ᴄᴀɴ'ᴛ ᴜsᴇ ᴛʜɪs ᴄᴍᴅ,, ʙᴜᴛ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ɪɴ ᴍʏ ᴘᴍ.")
+            return
+    query = event.pattern_match.group(1)
+    downloader.download(query, limit=5, output_dir='store', adult_filter_off=False, force_replace=False, timeout=60)
+    download_path = os.path.join('store', query)
+    os.chdir(download_path)
+    types = ('*.png', '*.jpeg', '*.jpg')
+    filesgrabbed = []
+    for file_type in types:
+        filesgrabbed.extend(glob.glob(file_type))
+    
+    # Send emoji message before images
+    await event.reply("Here are the images you requested! 💕")
+
+    # Send images
+    await event.client.send_file(event.chat_id, filesgrabbed, reply_to=event.id)
+    
+    # Delete images after sending
+    for file in filesgrabbed:
+        os.remove(file)
+    os.chdir('../../')
