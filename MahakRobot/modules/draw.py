1a906ad34a24f4642164b0f5dad1a5561826173d
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-31 14:47:22 +0530

    Update draw.py

--- MahakRobot/modules/draw.py
+++ MahakRobot/modules/draw.py
@@ -1,25 +1,29 @@
 from pyrogram import filters
 from pyrogram.types import  Message
 from pyrogram.types import InputMediaPhoto
-from MahakRobot import pbot as app
+from .. import pbot as  Roy, BOT_USERNAME
 from MukeshAPI import api
 from pyrogram.enums import ChatAction,ParseMode
 
-@app.on_message(filters.command("draw"))
-async def draw_(b, message: Message):
+@Roy.on_message(filters.command("draw"))
+async def imagine_(b, message: Message):
     if message.reply_to_message:
-        text=message.reply_to_message.text
+        text = message.reply_to_message.text
     else:
 
-        text=message.text.split(None, 1)[1]
-    app=await message.reply_text("🎨")
+        text =message.text.split(None, 1)[1]
+    roy=await message.reply_text( "`Please wait...,\n\nGenerating prompt .. ...`")
     try:
         await b.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
         x=api.ai_image(text)
-        with open("mahak.jpg", 'wb') as f:
+        with open("roy.jpg", 'wb') as f:
             f.write(x)
-        caption = f"""⬤ ᴅʀᴀᴡɪɴɢ ɢᴇɴ ʙʏ ➥ ๛ᴍ ᴀ ʜ ᴀ ᴋ ♡゙"""
-        await app.delete()
-        await message.reply_photo("nykaa.jpg",caption=caption,quote=True)
+        caption = f"""
+    💘sᴜᴄᴇssғᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇᴅ : {text}
+    ✨ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ : @{BOT_USERNAME}
+    🥀ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}
+    """
+        await roy.delete()
+        await message.reply_photo("mahak.jpg",caption=caption,quote=True)
     except Exception as e:
-        await app.edit_text(f"error {e}"),
+        await roy.edit_text(f"error {e}")
