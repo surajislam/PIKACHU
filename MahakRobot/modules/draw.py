fbb422301a47ec29d3e529e9a118ce8d13e31f5b
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-31 11:41:47 +0530

    Update modules

---
+++ MahakRobot/modules/draw.py
@@ -0,0 +1,25 @@
+from pyrogram import filters
+from pyrogram.types import  Message
+from pyrogram.types import InputMediaPhoto
+from MahakRobot import pbot as app
+from MukeshAPI import api
+from pyrogram.enums import ChatAction,ParseMode
+
+@app.on_message(filters.command("draw"))
+async def draw_(b, message: Message):
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    else:
+
+        text =message.text.split(None, 1)[1]
+    app=await message.reply_text( "✨")
+    try:
+        await b.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
+        x=api.ai_image(text)
+        with open("mahak.jpg", 'wb') as f:
+            f.write(x)
+        caption = f"""⬤ ᴅʀᴀᴡɪɴɢ ɢᴇɴ ʙʏ ➥ ๛ᴍ ᴀ ʜ ᴀ ᴋ ♡゙ """
+        await app.delete()
+        await message.reply_photo("nykaa.jpg",caption=caption,quote=True)
+    except Exception as e:
+        await app.edit_text(f"error {e}"),
