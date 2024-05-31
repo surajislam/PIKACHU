86f70ab14b25e47aeee41c8516170f2566e5d86a
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-31 14:05:21 +0530

    Update draw.py

--- MahakRobot/modules/draw.py
+++ MahakRobot/modules/draw.py
@@ -8,10 +8,10 @@ from pyrogram.enums import ChatAction,ParseMode
 @app.on_message(filters.command("draw"))
 async def draw_(b, message: Message):
     if message.reply_to_message:
-        text = message.reply_to_message.text
+        text=message.reply_to_message.text
     else:
 
-        text = message.text.split(None, 1)[1]
+        text=message.text.split(None, 1)[1]
     app=await message.reply_text("🎨")
     try:
         await b.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
