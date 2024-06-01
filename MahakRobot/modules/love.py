638774010ef7f862800fca9a80d0bccf538ab3e3
surajislam <154444093+surajislam@users.noreply.github.com>
2024-06-01 20:40:41 +0530

    Update love.py

--- MahakRobot/modules/love.py
+++ MahakRobot/modules/love.py
@@ -26,7 +26,7 @@ def get_random_message(love_percentage):
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Mahakxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/GALAKOO_USER_BOT?startgroup=true"),
     ],
 ]
 
